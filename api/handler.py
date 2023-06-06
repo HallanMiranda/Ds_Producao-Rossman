0ab4e3072a4e913709fc519a08b7f93a608e9123
Hallan Miranda <hallanmiranda@icloud.com>
2023-06-06 16:50:56 -0300

    revisando o deploy

--- api/handler.py
+++ api/handler.py
@@ -5,40 +5,37 @@ from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # Loading model
-model = pickle.load( open('/Users/hallanmiranda/Documents/repos/projeto_portfolio/pa003-Rossman/model/model_rossmann.pkl', 'rb') )
+model = pickle.load(open('/Users/hallanmiranda/Documents/repos/meus_projeto_portfolio/dsp_rossman/model/model_rossmann.pkl', 'rb'))
 
-# Inicialize API
+# Initialize API
 app = Flask( __name__ )
 
-@app.route( '/rossmann/predict', methods=['POST'] )
+@app.route('/rossmann/predict', methods=['POST'])
+def Rossomann_predict():
+    test_json = request.get_json()
 
-def rossomann_predict():
-    test_json = request.get_jason()
+    if test_json:  # There is data
+        if isinstance(test_json, dict):  # unique example
+            test_raw = pd.DataFrame(test_json, index=[0])
+        else:  # multiple examples
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
 
-    if test_json:# There is data
-        if isinstance( test_json, dict ):# unique exemple
-            test_raw = pd.DataFrame( test_json, index=[0] )
-        else: # multiple exmple
-            test_json = pd.DataFrame( test_json, columns=test_json[0].keys() )
-
-        # Instace Rossmann
+        # Instance Rossmann
         pipeline = Rossmann()
 
         # Data Clean
-        df1 = pipeline.data_cleaning( test_raw )
-        #Feacture Enginier
-        df2 = pipeline.feature_engineering( df1 )
-        #Data Preparation
-        df3 = pipeline.data_preparation( df2 )
-        #Prediction
-        df_response = pipeline.get_prediction( model, test_raw, df3)
+        df1 = pipeline.data_cleaning(test_raw)
+        # Feature Engineering
+        df2 = pipeline.feature_engineering(df1)
+        # Data Preparation
+        df3 = pipeline.data_preparation(df2)
+        # Prediction
+        df_response = pipeline.get_prediction(model, test_raw, df3)
 
         return df_response
-        
     else:
-        return Response( '{}', status = 200, mimetype= 'application/json')
+        return Response('{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
-
-
+    app.run(host='0.0.0.0', port=5000)
+    
--- api/rossmann/Rossmann.py
+++ api/rossmann/Rossmann.py
@@ -1,35 +1,38 @@
 import pickle
 import inflection
 import pandas as pd
-import numpy
+import numpy as np
 import math
 import datetime
 
-
-
-class Rossmann(object):
-    def __init__(self):
-        self.home_path = '/Users/hallanmiranda/Documents/repos/projeto_portfolio/pa003-Rossman'
-        self.competition_distace_scaler = pickle.load(open('parameter/competition_distance_scaler.pkl', 'rb'))
-        self.competition_time_month_scaler = pickle.load(open('parameter/competition_time_month_scaler.pkl', 'rb'))
-        self.promo_time_week_scaler = pickle.load(open('parameter/promo_time_week_scaler.pkl', 'rb'))
-        self.year_scaler = pickle.load(open('parameter/year_scaler.pkl', 'rb'))
-        self.store_type_scaler = pickle.load(open('parameter/store_type_scaler.pkl', 'rb'))
-                
+class Rossmann( object ):
+    def __init__( self ):
+        self.home_path='/Users/hallanmiranda/Documents/repos/meus_projeto_portfolio/dsp_rossman/'
+        self.competition_distance_scaler   = pickle.load( open( self.home_path + 'parameter/competition_distance_scaler.pkl', 'rb') )
+        self.competition_time_month_scaler = pickle.load( open( self.home_path + 'parameter/competition_time_month_scaler.pkl', 'rb') )
+        self.promo_time_week_scaler        = pickle.load( open( self.home_path + 'parameter/promo_time_week_scaler.pkl', 'rb') )
+        self.year_scaler                   = pickle.load( open( self.home_path + 'parameter/year_scaler.pkl', 'rb') )
+        self.store_type_scaler             = pickle.load( open( self.home_path + 'parameter/store_type_scaler.pkl', 'rb') )
         
-
-    def data_cleaning( self, df1):
-        # rename columns
-        cols_old = ['Store', 'DayOfWeek', 'Date','Open', 'Promo', 'StateHoliday','SchoolHoliday', 
+        
+    def data_cleaning( self, df1 ): 
+        
+        ## 1.1. Rename Columns
+        cols_old = ['Store', 'DayOfWeek', 'Date', 'Open', 'Promo', 'StateHoliday', 'SchoolHoliday', 
                     'StoreType', 'Assortment', 'CompetitionDistance', 'CompetitionOpenSinceMonth',
                     'CompetitionOpenSinceYear', 'Promo2', 'Promo2SinceWeek', 'Promo2SinceYear', 'PromoInterval']
-        # snakecase
-        cols_old = df1.columns.tolist()
-        cols_new = list(map(snakecase, cols_old))
+
+        snakecase = lambda x: inflection.underscore( x )
+
+        cols_new = list( map( snakecase, cols_old ) )
+
+        # rename
         df1.columns = cols_new
 
-         
-        # Fillout NA
+        ## 1.3. Data Types
+        df1['date'] = pd.to_datetime( df1['date'] )
+
+        ## 1.5. Fillout NA
         #competition_distance        
         df1['competition_distance'] = df1['competition_distance'].apply( lambda x: 200000.0 if math.isnan( x ) else x )
 
@@ -51,22 +54,23 @@ class Rossmann(object):
         df1['promo_interval'].fillna(0, inplace=True )
 
         df1['month_map'] = df1['date'].dt.month.map( month_map )
-         
+
         df1['is_promo'] = df1[['promo_interval', 'month_map']].apply( lambda x: 0 if x['promo_interval'] == 0 else 1 if x['month_map'] in x['promo_interval'].split( ',' ) else 0, axis=1 )
-        
-        # Change Data Types
+
+        ## 1.6. Change Data Types
         # competiton
         df1['competition_open_since_month'] = df1['competition_open_since_month'].astype( int )
         df1['competition_open_since_year'] = df1['competition_open_since_year'].astype( int )
-            
+
         # promo2
         df1['promo2_since_week'] = df1['promo2_since_week'].astype( int )
         df1['promo2_since_year'] = df1['promo2_since_year'].astype( int )
         
-        return df1
+        return df1 
 
 
-    def feature_engineering(self, df2):
+    def feature_engineering( self, df2 ):
+
         # year
         df2['year'] = df2['date'].dt.year
 
@@ -97,36 +101,44 @@ class Rossmann(object):
         # state holiday
         df2['state_holiday'] = df2['state_holiday'].apply( lambda x: 'public_holiday' if x == 'a' else 'easter_holiday' if x == 'b' else 'christmas' if x == 'c' else 'regular_day' )
 
-        # Filtragem das Variaveis
-        df2 = df2(df2['open'] != 0)
-   
-        # Selecao das colunas
+        # 3.0. PASSO 03 - FILTRAGEM DE VARIÁVEIS
+        ## 3.1. Filtragem das Linhas
+        df2 = df2[df2['open'] != 0]
+
+        ## 3.2. Selecao das Colunas
         cols_drop = ['open', 'promo_interval', 'month_map']
         df2 = df2.drop( cols_drop, axis=1 )
         
         return df2
 
-    def data_preparation(self, df5):
+
+    def data_preparation( self, df5 ):
+
+        ## 5.2. Rescaling 
         # competition distance
-        df5['competition_distance'] = self.competition_distace_scaler.fit_transform(df5[['competition_distance']].values)
+        df5['competition_distance'] = self.competition_distance_scaler.fit_transform( df5[['competition_distance']].values )
+    
         # competition time month
-        df5['competition_time_month'] = self.competition_time_month_scaler.fit_transform(df5[['competition_time_month']].values)
+        df5['competition_time_month'] = self.competition_time_month_scaler.fit_transform( df5[['competition_time_month']].values )
+
         # promo time week
-        df5['promo_time_week'] = self.promo_time_week_scaler.fit_transform(df5[['promo_time_week']].values)
+        df5['promo_time_week'] = self.promo_time_week_scaler.fit_transform( df5[['promo_time_week']].values )
+        
         # year
-        df5['year'] = self.year_scaler.fit_transform(df5[['year']].values)
-       
+        df5['year'] = self.year_scaler.fit_transform( df5[['year']].values )
+
+        ### 5.3.1. Encoding
         # state_holiday - One Hot Encoding
-        df5 = pd.get_dummies(df5, prefix=['state_holiday'], columns=['state_holiday']) 
+        df5 = pd.get_dummies( df5, prefix=['state_holiday'], columns=['state_holiday'] )
 
         # store_type - Label Encoding
-        le = LabelEncoder()
         df5['store_type'] = self.store_type_scaler.fit_transform( df5['store_type'] )
-        
+
         # assortment - Ordinal Encoding
         assortment_dict = {'basic': 1,  'extra': 2, 'extended': 3}
         df5['assortment'] = df5['assortment'].map( assortment_dict )
 
+        
         ### 5.3.3. Nature Transformation
         # day of week
         df5['day_of_week_sin'] = df5['day_of_week'].apply( lambda x: np.sin( x * ( 2. * np.pi/7 ) ) )
@@ -143,16 +155,21 @@ class Rossmann(object):
         # week of year
         df5['week_of_year_sin'] = df5['week_of_year'].apply( lambda x: np.sin( x * ( 2. * np.pi/52 ) ) )
         df5['week_of_year_cos'] = df5['week_of_year'].apply( lambda x: np.cos( x * ( 2. * np.pi/52 ) ) )
-
-        cols_selected = ['store','promo','store_type','assortment','competition_distance','competition_open_since_month','competition_open_since_year',
-                                'promo2','promo2_since_week','promo2_since_year','competition_time_month','promo_time_week','day_of_week_sin','day_of_week_cos',
-                                'month_sin','month_cos','day_sin','day_cos','week_of_year_sin','week_of_year_cos']
-        return df5[ cols_selectedc] 
-
-    def get_prediction(self, model, original_data, test_data):
-        # predition 
-        pred = model.presict( test_data )
+        
+        
+        cols_selected = [ 'store', 'promo', 'store_type', 'assortment', 'competition_distance', 'competition_open_since_month',
+            'competition_open_since_year', 'promo2', 'promo2_since_week', 'promo2_since_year', 'competition_time_month', 'promo_time_week',
+            'day_of_week_sin', 'day_of_week_cos', 'month_sin', 'month_cos', 'day_sin', 'day_cos', 'week_of_year_sin', 'week_of_year_cos']
+        
+        return df5[ cols_selected ]
+    
+    
+    def get_prediction( self, model, original_data, test_data ):
+        # prediction
+        pred = model.predict( test_data )
+        
         # join pred into the original data
         original_data['prediction'] = np.expm1( pred )
+        
+        return original_data.to_json( orient='records', date_format='iso' )
 
-        return original_data.to_json( orient= 'records', date_format='iso')
